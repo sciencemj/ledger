3d41a84d579bf0baf0050b6680d2b5948689fe27
sciencemj <sciencemj.park@gmail.com>
2024-05-09 15:56:17 +0900

    simple money add button added

--- src/page/Daily.js
+++ src/page/Daily.js
@@ -100,12 +100,12 @@ export default function Daily({ navigation }) {
                                            value={price} onChangeValue={setPrice} delimiter={','} separator={'.'} precision={0} prefix={'₩'} minValue={0}/>
                         </View>
                         <View className={'flex-row justify-center items-center m-2 rounded-lg bg-amber-50'}>
-                            <Pressable className='flex-1 items-center justify-center'onPress={() => {
+                            <Pressable className='flex-1 items-center justify-center' onPress={() => {
                                 setPrice(price + 50000);
                             }}>
                                 <Text className={'text-xl'}>₩50,000</Text>
                             </Pressable>
-                            <Pressable className='flex-1 border-l-[1px] border-l-black items-center justify-center'onPress={() => {
+                            <Pressable className='flex-1 border-l-[1px] border-l-black items-center justify-center' onPress={() => {
                                 setPrice(price + 10000);
                             }}>
                                 <Text className={'text-xl'}>₩10,000</Text>
@@ -115,7 +115,7 @@ export default function Daily({ navigation }) {
                             }}>
                                 <Text className={'text-xl'}>₩5,000</Text>
                             </Pressable>
-                            <Pressable className='flex-1 border-l-[1px] border-l-black items-center justify-center'onPress={() => {
+                            <Pressable className='flex-1 border-l-[1px] border-l-black items-center justify-center' onPress={() => {
                                 setPrice(price + 1000);
                             }}>
                                 <Text className={'text-xl'}>₩1,000</Text>
